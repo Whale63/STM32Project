e21e9229bdc2b3b524f94fe9d605dcc35bebcf4c
Whale Zhang <153888929+Whale63@users.noreply.github.com>
2026-06-16 10:14:17 +0800

    Delete 控创杯6 directory

--- 控创杯6/Hardware/infrared.h
+++
@@ -1,9 +0,0 @@
-#ifndef __INFRARED_H
-#define __INFRARED_H
-
-int get_sensor_status(void); 
-int counter(void);
-void IR_Posture_Init(void);
-int IR_Posture_distinction(void);
-
-#endif
--- 控创杯6/Object/core_cm3.d
+++
@@ -1,2 +0,0 @@
-..\object\core_cm3.o: ..\Core\core_cm3.c
-..\object\core_cm3.o: D:\Keil uVision5\ARM\ARMCC\Bin\..\include\stdint.h
