2aed3ad2a7f73808ea066f1a6f7ddb9c564a2211
Whale Zhang <153888929+Whale63@users.noreply.github.com>
2026-06-16 10:14:44 +0800

    Delete 控创杯4 directory

--- 控创杯4/Hardware/infrard.c
+++
@@ -1,4 +0,0 @@
-#include "stm32f10x.h"                  // Device header
-
-
-void Posture_distingc
--- 控创杯4/Hardware/infrared.h
+++
@@ -1,7 +0,0 @@
-#ifndef __INFRARED_H
-#define __INFRARED_H
-
-void IR_Posture_Init(void);
-int IR_Posture_distinction(void);
-
-#endif
